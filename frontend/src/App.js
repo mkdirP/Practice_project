86ceb2460f0c8c07c843559bb9705e88447f84b3
MkdirP <336262@niuitmo.ru>
2025-04-12 20:47:12 +0300

    frontend first
    不含pdf上传功能和统计功能的前端实现（删除了node_modules目录）

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import UploadPage from "./component/UploadPage";
+
+function App() {
+  return (
+      <div className="App">
+        <UploadPage />
+      </div>
+  );
+}
+
+export default App;
